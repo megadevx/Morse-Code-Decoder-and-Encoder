f401b8b343aefef3f64832f71429d45550f6a726
Devin Ferguson <dbfnrf@mail.umkc.edu>
2017-11-13 16:42:43 -0600

    Added the class encoder outline

--- Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/Encoder.h
+++ Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/Encoder.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <map>
+#include <string>
+
+
+class encoder {
+private:
+	std::map<char, std::string> alphabet;
+	std::string input;
+	void create_alphabet();
+public:
+	encoder();
+	std::string encode(std::string input);
+
+	std::string get_input();
+	void set_input(std::string in);
+};
