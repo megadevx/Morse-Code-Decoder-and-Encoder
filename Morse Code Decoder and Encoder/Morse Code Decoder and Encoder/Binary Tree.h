6a2580df20770cc8a99a3345d5c7f7d1d327fafd
mjadrales <mjadrales12@gmail.com>
2017-11-13 20:49:33 -0600

    Added outline for decoder class

--- Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/Binary Tree.h
+++ Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/Binary Tree.h
@@ -1,1 +1,41 @@
 #pragma once
+//Binary Tree Class, courtesy of Dr Kuhail's source code
+
+/** A node for a Binary Tree. */
+template<typename Item_Type>
+struct BTNode
+{
+	// Data Fields
+	Item_Type data;
+	BTNode<Item_Type>* left;
+	BTNode<Item_Type>* right;
+
+	// Constructor
+	BTNode(const Item_Type& the_data,
+		BTNode<Item_Type>* left_val = NULL,
+		BTNode<Item_Type>* right_val = NULL) :
+		data(the_data), left(left_val), right(right_val) {}
+
+	// Destructor (to avoid warning message)
+	virtual ~BTNode() {}
+	// to_string
+	virtual std::string to_string() const {
+		std::ostringstream os;
+		os << data;
+		return os.str();
+	}
+}; // End BTNode
+
+template<typename Item_Type>
+class Binary_Tree
+{
+
+
+	Binary_Tree(BTNode<Item_Type>* new_root) :
+		root(new_root) {}
+
+	// Data Field
+	BTNode<Item_Type>* root;
+
+};
+
--- Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/Decoder.h
+++ Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/Decoder.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "Binary Tree.h"
+#include <string>
+using namespace std;
+
+class Decoder {
+private:
+	BTNode<string> *root = nullptr;
+
+public:
+	Decoder();
+	Binary_Tree<string> build_tree();
+	string decode(string in);
+};
