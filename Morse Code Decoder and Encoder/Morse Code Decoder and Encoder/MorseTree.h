01ae419330b8b56f63fdc6a34310be1e3cfcb559
Devin Ferguson <dbfnrf@mail.umkc.edu>
2017-11-18 14:19:57 -0600

    Fixed the morse tree from having two trees

--- Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/MorseTree.h
+++ Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/MorseTree.h
@@ -5,22 +5,16 @@
 using namespace std;
 
 class MorseTree : public Binary_Tree {		//Derived class MorseTree from Binary_Tree
-private:
-	Binary_Tree tree;
 
 public:
-	//Default constructor
-	MorseTree(){
-		tree = Binary_Tree();
-	}
 
 	//Member funtions
 	void add_letter(string line) {
 		BTNode *insert = new BTNode(line);
 
 		//Takes in an input string, gets the letter (first character) and places into the tree
-		if (tree.get_root() == NULL) {
-			tree.set_root(insert);
+		if (this->get_root() == NULL) {
+			this->set_root(insert);
 		}
 	}
 };
