0e9d3033b9a23daf0520dd3c792365a846270254
Morgan Adrales <mjadrales12@gmail.com>
2017-11-21 21:09:19 -0600

    Revert "Merge branch 'master' of https://github.com/megadevx/Morse-Code-Decoder-and-Encoder"
    This reverts commit 9da5eb2f59b9fc56bc2fb47f9f868eb022572f82, reversing
    changes made to e7b238c7133385acf8a5e11dc8c3603ec6f7a972.

--- Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/MorseTree.h
+++ Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/MorseTree.h
@@ -5,7 +5,6 @@
 using namespace std;
 
 class MorseTree : public Binary_Tree {		//Derived class MorseTree from Binary_Tree
-<<<<<<< HEAD
 private:
 
 public:
@@ -13,17 +12,12 @@ public:
 	MorseTree(){
 		Binary_Tree();
 	}
-=======
-
-public:
->>>>>>> 01ae419330b8b56f63fdc6a34310be1e3cfcb559
 
 	//Member funtions
 	BTNode *get_root() {
 		return root;
 	}
 
-<<<<<<< HEAD
 	void add_letter(BTNode *node, string code, string letter) {
 		//Get the first character, create node with that data
 		if (node == NULL) {
@@ -37,11 +31,6 @@ public:
 		}
 		else if (code[0] == '_') {
 			add_letter(node->right, code.substr(1), letter);
-=======
-		//Takes in an input string, gets the letter (first character) and places into the tree
-		if (this->get_root() == NULL) {
-			this->set_root(insert);
->>>>>>> 01ae419330b8b56f63fdc6a34310be1e3cfcb559
 		}
 	}
 };
