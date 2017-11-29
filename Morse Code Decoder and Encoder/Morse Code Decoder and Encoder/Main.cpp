d34788da658f219364f5fb49be04159b0ae24dea
mjadrales <mjadrales12@gmail.com>
2017-11-29 13:23:57 -0600

    Fixed text (from Github push) in main causing errors

--- Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/Main.cpp
+++ Morse Code Decoder and Encoder/Morse Code Decoder and Encoder/Main.cpp
@@ -4,12 +4,9 @@ using namespace std;
 void main() {
 	Morse_Converter MC;
 	MC.parse();
-<<<<<<< HEAD
 	MC.decode("._. . _");
-=======
 	string result1 = MC.decode("._.");
 	string result2 = MC.encode("cat");
 	cout << result1 << " " << result2 << endl;
->>>>>>> 4c8ce38322e241293bb7aac3cd23145192575cd6
 	system("PAUSE");
 }
